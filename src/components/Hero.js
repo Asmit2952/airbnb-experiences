b20b3423fc8a6081d7ed53e163b71c5eb4610e76
Asmit2952 <asmitbm2952002@gmail.com>
2022-06-29 11:29:25 +0530

    modified hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,8 +3,12 @@ import photogrid from "../images/photo-grid.png"
 export default function Hero () {
     return (
         <div>
-            <div className="air-grid">
-                <img src={photogrid} alt="airbnb grid" />
+            <div className="photog">
+                <img src={photogrid} alt="airbnb grid" className="air-grid"/>
+            </div>
+            <div className="info">
+                <h2>Online Experiences</h2>
+                <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
             </div>
         </div>
     )
